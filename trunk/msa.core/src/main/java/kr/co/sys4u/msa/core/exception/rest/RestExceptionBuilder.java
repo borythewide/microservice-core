5565c17f0661df1ff1f791c6e7bef56f115890c5
skson <skson@ac42073e-2228-4018-90a0-9e8d426ed4a5>
2017-06-20 09:14:28 +0000

    exception builder 개발

--- trunk/msa.core/src/main/java/kr/co/sys4u/msa/core/exception/rest/RestExceptionBuilder.java
+++ trunk/msa.core/src/main/java/kr/co/sys4u/msa/core/exception/rest/RestExceptionBuilder.java
@@ -9,57 +9,108 @@ import org.springframework.web.client.HttpClientErrorException;
 import kr.co.sys4u.msa.core.exception.AlertException;
 
 public class RestExceptionBuilder {
+
+	private List<RestExceptionContext> restExceptionInfos = new ArrayList<>();
 	private HttpClientErrorException cause;
-	private List<HttpStatus> httpStatuses = new ArrayList<>();
-	private String message;
-	private String redirectUrl;
-	private Class<? extends AlertException> exceptionClass;
-	
-	private RestExceptionBuilder(){
+	private RestExceptionContext currentContext;
+
+	private RestExceptionBuilder() {
 		super();
 	}
-	
-	public static RestExceptionBuilder currentException(HttpClientErrorException cause){
-		RestExceptionBuilder builder = new RestExceptionBuilder();
-		builder.cause = cause;
-		return builder;
+
+	public static RestExceptionBuilder create(HttpClientErrorException cause) {
+		RestExceptionBuilder restExceptionBuilder = new RestExceptionBuilder();
+		restExceptionBuilder.cause = cause;
+
+		return restExceptionBuilder;
 	}
-	
-	public RestExceptionBuilder when(HttpStatus httpStatus){
-		httpStatuses.add(httpStatus);
+
+	public RestExceptionBuilder when( HttpStatus httpStatus) {
+		currentContext = new RestExceptionContext();
+		currentContext.getHttpStatus().add(httpStatus);
+
+		restExceptionInfos.add(currentContext);
+		return this;
+	}
+
+	public RestExceptionBuilder andWhen(HttpStatus httpStatus) {
+		currentContext.getHttpStatus().add(httpStatus);
+		return this;
+	}
+
+	public RestExceptionBuilder thenThrow(Class<? extends AlertException> exceptionClass) {
+		currentContext.setExceptionClass(exceptionClass);
+		return this;
+	}
+
+	public RestExceptionBuilder message(String message) {
+		currentContext.setMessage(message);
+		return this;
+	}
+
+	public RestExceptionBuilder noMessage() {
+		currentContext.setMessage(null);
 		return this;
 	}
 	
-	public RestExceptionBuilder message(String message){
-		this.message = message;
+	public RestExceptionBuilder redirectUrl(String redirectUrl) {
+		currentContext.setRedirectUrl(redirectUrl);
 		return this;
 	}
-	
-	public RestExceptionBuilder redirectUrl(String redirectUrl){
-		this.redirectUrl = redirectUrl;
-		return this;
-	}
-	
-	public RestExceptionBuilder thenThrow(Class<? extends AlertException> exceptionClass){
-		this.exceptionClass = exceptionClass;
-		return this;
-	}
-	
-	public RuntimeException build(){
+
+	public RuntimeException build() {
 		HttpStatus currentStatus = this.cause.getStatusCode();
-		
-		if(httpStatuses.contains(currentStatus)){
-			return newException();
+
+		for (RestExceptionContext restExceptionInfo : restExceptionInfos) {
+			if (restExceptionInfo.getHttpStatus().contains(currentStatus)) {
+				try {
+					return restExceptionInfo.getExceptionClass()
+							.getConstructor(String.class, String.class, Throwable.class)
+							.newInstance(restExceptionInfo.getMessage(), restExceptionInfo.getRedirectUrl(), cause);
+				} catch (Exception e) {
+					/* ignored */
+				}
+
+			}
 		}
-		
-		return new RestException(message);
+
+		return new RestException(cause.getMessage());
 	}
-	
-	private RuntimeException newException(){
-		try {
-			return exceptionClass.getConstructor(String.class, String.class, Throwable.class).newInstance(message, redirectUrl, cause);
-		} catch (Exception e) {
-			throw new IllegalStateException(e);
+
+	private static class RestExceptionContext {
+		private List<HttpStatus> httpStatus = new ArrayList<>();
+		private String message = "오류가 발생하였습니다.";
+		private String redirectUrl = "/index/index.do";
+		private Class<? extends AlertException> exceptionClass = AlertException.class;
+
+		public List<HttpStatus> getHttpStatus() {
+			return httpStatus;
 		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		public String getRedirectUrl() {
+			return redirectUrl;
+		}
+
+		public void setRedirectUrl(String redirectUrl) {
+			this.redirectUrl = redirectUrl;
+		}
+
+		public Class<? extends AlertException> getExceptionClass() {
+			return exceptionClass;
+		}
+
+		public void setExceptionClass(Class<? extends AlertException> exceptionClass) {
+			this.exceptionClass = exceptionClass;
+		}
+
 	}
+
 }
